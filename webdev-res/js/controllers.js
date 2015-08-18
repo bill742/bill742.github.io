7b6e6193768920077ccdcb0a766d5cf6aecc28be
bill742 <742designs@gmail.com>
2015-08-17 23:58:29 -0400

    Adding WebDev Resources app demo

---
+++ webdev-res/js/controllers.js
@@ -0,0 +1,11 @@
+(function(){
+	var linkControllers = angular.module('linkControllers', []);
+
+	// Display submitted links
+	linkControllers.controller('ListController', ['$scope', '$http', function($scope, $http){
+		$http.get('js/data.json').success(function(links) {
+			$scope.products = links;
+		});
+	}]);
+
+})();
