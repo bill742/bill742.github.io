1070661fd10572136bf9329bde9777e30ea55d3f
Bill Dean <742designs@gmail.com>
2017-05-25 17:17:57 -0400

    Updating copy. Gradually removing font awesome icons

--- js/scripts.js
+++ js/scripts.js
@@ -2,40 +2,40 @@ jQuery(document).ready(function($) {
 
     // Navagation scrolling
     $('nav a.navScroll').click(function() {
-        var $target = $(this.hash);
-        var targetOffset = $target.offset().top;
-        $('html, body').animate({
-            scrollTop: targetOffset
-        }, 700);
+      var $target = $(this.hash);
+      var targetOffset = $target.offset().top;
+      $('html, body').animate({
+        scrollTop: targetOffset
+      }, 700);
 
-        if ($('nav').hasClass('mobile')) {
-            $('nav').slideToggle();
-            $('.menu-btn').show();
-            $('.close').hide();
-        }
+      if ($('nav').hasClass('mobile')) {
+        $('nav').slideToggle();
+        $('.menu-btn').show();
+        $('.close').hide();
+      }
 
-        return false;
+      return false;
     });
 
     // Hamburger menu
     $('.menu-btn').click(function() {
-        $('nav').slideToggle().addClass('mobile');
-        $('.menu-btn').hide();
-        $('.close').show();
+      $('nav').slideToggle().addClass('mobile');
+      $('.menu-btn').hide();
+      $('.close').show();
     });
 
     $('.close').click(function() {
-        $('nav').slideToggle();
-        $('.menu-btn').show();
-        $('.close').hide();
+      $('nav').slideToggle();
+      $('.menu-btn').show();
+      $('.close').hide();
     });
 
     // Portfolio rollover
     $('.port-copy').click(function() {
-        $(this).toggleClass('slide');
-        $(this).children('i').toggle();
-        $(this).children('p').toggle();
-        $(this).siblings('.port-img').toggleClass('img-hover');
+      $(this).toggleClass('slide');
+      $(this).children('span').toggle();
+      $(this).children('p').toggle();
+      $(this).siblings('.port-img').toggleClass('img-hover');
     });
 
 }); // End DocReady
