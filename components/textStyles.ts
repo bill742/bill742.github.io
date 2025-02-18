86f5cdf0d2708b2d91b03e5db385992686c154b1
Bill Dean <742designs@gmail.com>
2025-02-18 15:27:01 -0500

    Restructure styles and favicon.

--- components/textStyles.ts
+++
@@ -1,9 +0,0 @@
-// import { mode } from '@chakra-ui/theme-tools';
-
-export const TextStyles = {
-  baseStyle: () => ({
-    // color: mode('primaryDark', 'primaryLight'),
-  }),
-  defaultProps: {},
-  sizes: {},
-};
