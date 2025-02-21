1881cc058f315760f083447e9407f90d29d15804
Bill Dean <742designs@gmail.com>
2025-02-21 15:38:37 -0500

    Test adding vars.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_GH_USERNAME: process.env.NEXT_PUBLIC_GH_USERNAME,
+    NEXT_PUBLIC_SITE_TITLE: process.env.NEXT_PUBLIC_SITE_TITLE,
+  },
 };
 
 export default nextConfig;
