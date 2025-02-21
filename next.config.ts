4e58422f7855ae87d96d0a6b09d738e98a9f27f3
Bill Dean <742designs@gmail.com>
2025-02-20 23:07:36 -0500

    Remove unnecessary config option.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+  output: "export",
 };
 
 export default nextConfig;
