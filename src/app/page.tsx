6a60da37cd4e6b8ba1cca929102505721e435b78
Bill Dean <742designs@gmail.com>
2025-02-21 10:44:51 -0500

    Temporarily remove content.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-import About from "@/components/Sections/About";
+// import About from "@/components/Sections/About";
 // import Contact from "@/components/Sections/Contact";
 // import Portfolio from "@/components/Sections/Portfolio";
 // import Skills from "@/components/Sections/Skills";
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   // },
   description:
     "Hello World! My name is Bill Dean and I'm a web developer based in Toronto, Canada. I specialize in building websites and applications using modern JavaScript with React, NextJS and TypeScript.",
-  metadataBase: new URL(`${process.env.NEXT_PUBLIC_APP_BASE_URL}`),
+  // metadataBase: new URL(`${process.env.NEXT_PUBLIC_APP_BASE_URL}`),
   title: `${process.env.NEXT_PUBLIC_SITE_TITLE}`,
 };
 
@@ -20,7 +20,8 @@ export default function Home() {
 
   return (
     <main className="row-start-2 mb-16 mt-16 flex flex-col items-center gap-20 sm:items-start">
-      <About />
+      Hello!
+      {/* <About /> */}
       {/* <Skills />
       {portfolio && <Portfolio />}
       <Contact /> */}
