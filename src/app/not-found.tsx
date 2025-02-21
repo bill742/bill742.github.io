6c28d692923d7667f4f095d6773bf4d1d2497029
Bill Dean <742designs@gmail.com>
2025-02-21 10:17:58 -0500

    Temporarily remove link.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,9 +5,9 @@ export default function NotFound() {
     <div className="flex flex-col items-center gap-4">
       <h2 className="text-3xl font-bold">Whoops!</h2>
       <p className="text-xl">You seem to have found a broken link.</p>
-      <Link href="/" className="text-xl underline">
+      {/* <Link href="/" className="text-xl underline">
         Return Home
-      </Link>
+      </Link> */}
     </div>
   );
 }
