d31935fd557c9d14d22f0a407841cc7b3ac934d5
Bill Dean <742designs@gmail.com>
2025-03-05 17:59:06 -0500

    Update config to write static sitemap.xml.

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_GH_USERNAME: process.env.NEXT_PUBLIC_GH_USERNAME,
     NEXT_PUBLIC_SITE_TITLE: process.env.NEXT_PUBLIC_SITE_TITLE,
   },
+  async rewrites() {
+    return [
+      {
+        destination: "/api/sitemap",
+        source: "/sitemap.xml",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
