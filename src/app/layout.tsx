80a4233b9846e8b2109cf1a79d307ca627b58ea6
Bill Dean <742designs@gmail.com>
2025-03-05 14:43:58 -0500

    Add additional tracking for analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,10 +42,20 @@ export default function RootLayout({
           <div id="main">{children}</div>
         </div>
         {/* 100% privacy-first analytics */}
+
         <script
+          data-collect-dnt="true"
           async
           src="https://scripts.simpleanalyticscdn.com/latest.js"
         ></script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="https://queue.simpleanalyticscdn.com/noscript.gif?collect-dnt=true"
+            alt=""
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </noscript>
       </body>
     </html>
   );
