b0f8518b717ec4e7714602c8d7f16033b93957c8
Bill Dean <742designs@gmail.com>
2025-03-06 12:09:17 -0500

    Resolve issues with sitemap failing to build.

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,7 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_GH_USERNAME: process.env.NEXT_PUBLIC_GH_USERNAME,
     NEXT_PUBLIC_SITE_TITLE: process.env.NEXT_PUBLIC_SITE_TITLE,
   },
-  async rewrites() {
-    return [
-      {
-        destination: "/api/sitemap",
-        source: "/sitemap.xml",
-      },
-    ];
-  },
+  output: "export",
 };
 
 export default nextConfig;
