8d695270953fcf6c59825a7a734ca16e15944789
Bill Dean <742designs@gmail.com>
2025-03-23 21:21:29 -0400

    Add imageLoader config to resolve issue with Resume image.

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_GH_USERNAME: process.env.NEXT_PUBLIC_GH_USERNAME,
     NEXT_PUBLIC_SITE_TITLE: process.env.NEXT_PUBLIC_SITE_TITLE,
   },
+  images: {
+    loader: "custom",
+    loaderFile: "./imageLoader.js",
+  },
   output: "export",
 };
 
