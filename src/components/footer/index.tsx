94597ce5a2005e207a907de5d856d03a74b3fdca
Bill Dean <742designs@gmail.com>
2025-09-24 16:17:32 -0400

    Update footer index file

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,8 +2,8 @@ import UpArrow from "./up-arrow";
 
 export default function Footer() {
   return (
-    <>
+    <footer className="flex justify-end" aria-label="Footer">
       <UpArrow />
-    </>
+    </footer>
   );
 }
