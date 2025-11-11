d26e96eb7e794a9cbbaff88b1e40487d0dac1d6b
Bill Dean <742designs@gmail.com>
2025-11-11 13:36:03 -0500

    Minor tweaks.

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,5 +1,5 @@
-import { expect, test } from "@playwright/test";
 import AxeBuilder from "@axe-core/playwright";
+import { expect, test } from "@playwright/test";
 
 test.describe("Homepage does not have accessiblity issues", () => {
   test("Should not have any automatically detectable accessibility issues", async ({
@@ -16,6 +16,6 @@ test.describe("Homepage does not have accessiblity issues", () => {
 test("Home page displays correct metadata", async ({ page }) => {
   await page.goto("./");
 
-  // Expect a title "to contain" a substring.
+  // Expect page title to be the same as in env variable
   await expect(page).toHaveTitle(process.env.NEXT_PUBLIC_SITE_TITLE || "");
 });
