d901a5b14835b6c33e0a95d8a2336bfd7808549c
Bill Dean <742designs@gmail.com>
2025-11-16 18:18:46 -0500

    Add additional tests.

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -24,3 +24,24 @@ test("Home page displays correct metadata", async ({ page }) => {
   // Expect page title to be the same as in env variable
   await expect(page).toHaveTitle(process.env.NEXT_PUBLIC_SITE_TITLE || "");
 });
+
+test("Home page displays correct headings", async ({ page }) => {
+  await page.goto("./");
+
+  console.log("Checking headings on homepage");
+
+  // Expect main heading to be the same as in env variable
+  const mainHeading = page.locator("h1").first();
+  const subHeading = page.locator("h2").first();
+  await expect(mainHeading).toHaveText("Hello! My name is Bill.");
+  await expect(subHeading).toHaveText("I'm a front-end web developer.");
+});
+
+test("Home page displays About section correctly", async ({ page }) => {
+  await page.goto("./");
+
+  console.log("Checking About section on homepage");
+
+  await page.getByRole("button", { name: "About Me" }).click();
+  await expect(page.getByRole("heading", { name: "About Me" })).toBeVisible();
+});
