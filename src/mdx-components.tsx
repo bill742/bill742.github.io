e09b81d6cc4e5eec64480a9a0599af771421e973
Bill Dean <742designs@gmail.com>
2026-02-23 18:32:01 -0500

    Update element styles for markdown pages.

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,20 +1,24 @@
 import type { MDXComponents } from "mdx/types";
 import Image, { ImageProps } from "next/image";
 
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    // Allows customizing built-in components, e.g. to add styling.
-    h1: ({ children }) => (
-      <h1 style={{ color: "red", fontSize: "48px" }}>{children}</h1>
-    ),
-    img: (props) => (
-      <Image
-        sizes="100vw"
-        style={{ height: "auto", width: "100%" }}
-        {...(props as ImageProps)}
-        alt=""
-      />
-    ),
-    ...components,
-  };
+const components = {
+  // Allows customizing built-in components, e.g. to add styling.
+  h2: ({ children }) => (
+    <h2 style={{ fontSize: "1.5rem", marginBottom: "1rem" }}>{children}</h2>
+  ),
+  img: (props) => (
+    <Image
+      sizes="100vw"
+      style={{ height: "auto", width: "100%" }}
+      {...(props as ImageProps)}
+      alt=""
+    />
+  ),
+  p: ({ children }) => (
+    <p style={{ fontSize: "1.125rem", marginBottom: "1rem" }}>{children}</p>
+  ),
+} satisfies MDXComponents;
+
+export function useMDXComponents(): MDXComponents {
+  return components;
 }
