5551dcf9c7199f5736e043c94a292b1f7cd21db5
Bill Dean <742designs@gmail.com>
2026-03-31 17:05:01 -0400

    Update paths in robots and sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,14 +3,18 @@ import { MetadataRoute } from "next";
 export const dynamic = "force-static";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    return [
-        {
-            lastModified: new Date(),
-            url: "https://billdean.me",
-        },
-        {
-            lastModified: new Date(),
-            url: "https://billdean.me/resume",
-        },
-    ];
+  return [
+    {
+      lastModified: new Date(),
+      url: "https://billdean.me",
+    },
+    // {
+    //   lastModified: new Date(),
+    //   url: "https://billdean.me/resume",
+    // },
+    // {
+    //   lastModified: new Date(),
+    //   url: "https://billdean.me/blog",
+    // },
+  ];
 }
