27f8f6beeaaa3fb79a5cd3b10ee2de3eb8394989
Bill Dean <742designs@gmail.com>
2026-03-31 17:05:26 -0400

    Fix issues found by react doctor.

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,17 +1,24 @@
+import Image from "next/image";
+import Script from "next/script";
+
 const Analytics = () => {
   return (
     <>
       {/* 100% privacy-first analytics */}
-      <script
+      <Script
+        id="analytics"
+        strategy="afterInteractive"
         data-collect-dnt="true"
         async
         data-hostname="billdean.me"
         src="https://scripts.simpleanalyticscdn.com/latest.js"
-      ></script>
+      ></Script>
+
       <noscript>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src="https://queue.simpleanalyticscdn.com/noscript.gif?collect-dnt=true"
+        <Image
+          src={
+            "https://queue.simpleanalyticscdn.com/noscript.gif?collect-dnt=true"
+          }
           alt=""
           referrerPolicy="no-referrer-when-downgrade"
         />
