2bbd442b1f87c710eba349ab0f6e29e1f707e970
Bill Dean <742designs@gmail.com>
2026-03-31 17:06:22 -0400

    Minor linting fixes.

--- src/components/resumeSections/overview.tsx
+++ src/components/resumeSections/overview.tsx
@@ -6,8 +6,16 @@ const Overview: FC = memo(() => (
     <h2 className="resumeHeader">
       <FaUser className="text-primary" /> Overview
     </h2>
-    <p className="text-lg leading-relaxed text-muted-foreground">
-      Innovative and detail-oriented React Developer with a strong foundation in front-end development, specializing in building dynamic and responsive user interfaces. Proficient in React.js, JavaScript, TypeScript, and modern UI frameworks, with experience in state management, API integration, and performance optimization. Passionate about crafting seamless user experiences and writing clean, maintainable code. Adept at collaborating with cross-functional teams to develop scalable web applications. Seeking an opportunity to leverage my expertise in React development to contribute to a forward-thinking organization.
+    <p className="text-muted-foreground text-lg leading-relaxed">
+      Innovative and detail-oriented React Developer with a strong foundation in
+      front-end development, specializing in building dynamic and responsive
+      user interfaces. Proficient in React.js, JavaScript, TypeScript, and
+      modern UI frameworks, with experience in state management, API
+      integration, and performance optimization. Passionate about crafting
+      seamless user experiences and writing clean, maintainable code. Adept at
+      collaborating with cross-functional teams to develop scalable web
+      applications. Seeking an opportunity to leverage my expertise in React
+      development to contribute to a forward-thinking organization.
     </p>
   </section>
 ));
