fff4f58e070d495d44882a15b999ea2c1b8fa990
Bill Dean <742designs@gmail.com>
2026-04-09 15:26:39 -0400

    Implement accordion to project cards.

--- src/components/portfolio-card.tsx
+++ src/components/portfolio-card.tsx
@@ -1,16 +1,9 @@
+import Image from "next/image";
 import Link from "next/link";
 import { FC, memo } from "react";
 import { SiGithub } from "react-icons/si";
 
 import { Button } from "./ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "./ui/card";
 
 const PortfolioCard: FC<{
   title: string;
@@ -19,22 +12,35 @@ const PortfolioCard: FC<{
   githubLink?: string;
   demoLink?: string;
   techStack?: string;
+  image?: string;
 }> = memo(
-  ({ title, description, content, githubLink, demoLink, techStack }) => {
+  ({ title, description, content, githubLink, demoLink, techStack, image }) => {
     return (
-      <Card className="card-hover bg-card border-border hover:border-primary/30 justify-between">
-        <CardHeader>
-          <CardTitle className="text-foreground text-lg">{title}</CardTitle>
-          <CardDescription className="text-muted-foreground text-base">
-            {description}
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="text-muted-foreground leading-relaxed">
-          {content}
+      <div className="text-muted-foreground flex flex-col gap-y-4 text-[1rem] leading-relaxed">
+        <span className="text-muted-foreground text-base font-semibold">
+          {description}
+        </span>
 
-          {techStack && <div className="mt-4">Tech stack: {techStack}</div>}
-        </CardContent>
-        <CardFooter className="flex flex-col gap-8 sm:flex-row">
+        {image && (
+          <div className="relative h-auto min-h-44 w-full md:min-h-80">
+            <Image
+              src={`/images/projects/${image}`}
+              alt={title}
+              fill
+              className="object-cover object-top"
+            />
+          </div>
+        )}
+
+        {content}
+
+        {techStack && (
+          <div>
+            <span className="font-semibold">Tech stack:</span> {techStack}
+          </div>
+        )}
+
+        <div className="flex flex-col gap-8 sm:flex-row">
           {githubLink && (
             <Button asChild className="button-primary">
               <Link
@@ -60,8 +66,8 @@ const PortfolioCard: FC<{
               </Link>
             </Button>
           )}
-        </CardFooter>
-      </Card>
+        </div>
+      </div>
     );
   }
 );
