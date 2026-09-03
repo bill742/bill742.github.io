8392803f69e426ff84a23b3dc5cd460b6a4ec434
Bill Dean <742designs@gmail.com>
2026-09-03 16:40:10 -0400

    Adding Person schema. Misc copy updates.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,22 @@ import Contact from "@/components/homeSections/contact";
 import Intro from "@/components/homeSections/intro";
 import Portfolio from "@/components/homeSections/projects";
 import Skills from "@/components/homeSections/skills";
+import { JsonLd } from "@/components/JsonLd";
 import ScrollOnLoad from "@/components/scrollOnLoad";
+import { homeGraph } from "@/lib/schema";
 
 export const metadata: Metadata = {
   alternates: {
     canonical: `${process.env.NEXT_PUBLIC_SITE_URL}`,
   },
   description:
-    "I'm a front-end web developer based in Toronto, Canada. I specialize in building websites and applications using modern JavaScript with React, NextJS and TypeScript.",
+    "I'm a web developer based in Toronto, Canada. I specialize in building websites and applications using modern JavaScript with React, NextJS and TypeScript.",
 };
 
 export default function Home() {
   return (
     <div className="row-start-2 mt-16 flex flex-col items-center gap-20 sm:items-start">
+      <JsonLd schema={homeGraph} />
       <Suspense>
         <ScrollOnLoad />
       </Suspense>
